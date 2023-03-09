98eb0f67af415098384d5d65920dc1fba33843fc
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-03-09 22:40:09 +0100

    feat: add validation middleware
    With the use of the Joi library, data is tested to ensure it follows the
    defined schema in the application before getting forwarded to the
    controllers for processing and storage.

--- src/routes/api/Author.ts
+++ src/routes/api/Author.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import authorController from '../../controllers/Author';
+import { Schemas, ValidateSchema } from '../../middleware/ValidateSchema';
 
 const author = express.Router();
-author.post('/create', authorController.createAuthor);
+author.post('/create', ValidateSchema(Schemas.author.create), authorController.createAuthor);
 author.get('/get/:authorId', authorController.readAuthor);
 author.get('/get/', authorController.readAllAuthor);
-author.patch('/update/:authorId', authorController.updateAuthor);
+author.patch('/update/:authorId', ValidateSchema(Schemas.author.update), authorController.updateAuthor);
 author.delete('/delete/:authorId', authorController.deleteAuthor);
 
 export = author;
--- src/routes/api/Book.ts
+++ src/routes/api/Book.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import bookController from '../../controllers/Book';
+import { Schemas, ValidateSchema } from '../../middleware/ValidateSchema';
 
 const book = express.Router();
-book.post('/create', bookController.createBook);
+book.post('/create', ValidateSchema(Schemas.book.create), bookController.createBook);
 book.get('/get/:bookId', bookController.readBook);
 book.get('/get/', bookController.readAllBook);
-book.patch('/update/:bookId', bookController.updateBook);
+book.patch('/update/:bookId', ValidateSchema(Schemas.book.update), bookController.updateBook);
 book.delete('/delete/:bookId', bookController.deleteBook);
 
 export = book;
